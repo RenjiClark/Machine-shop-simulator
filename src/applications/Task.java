284647265d03b7e501856df8956b0c2a124314ef
Alex Gunness <gunn0056>
2014-01-29 15:46:01 -0600

    Task pulled out as a seperate class. Surprisingly the only time it is references is "new Task" so no further refactorings were needed.

---
+++ src/applications/Task.java
@@ -0,0 +1,14 @@
+package applications;
+
+// top-level nested classes
+class Task {
+    // data members
+    int machine;
+    int time;
+
+    // constructor
+    Task(int theMachine, int theTime) {
+        machine = theMachine;
+        time = theTime;
+    }
+}
